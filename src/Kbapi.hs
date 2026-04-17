e2346d4d6cd10893adabd8168b61d360088a83f5
OrenGitHub <tuna.is.good.for.you@gmail.com>
2026-04-17 19:19:31 +0300

    initial documentation

--- src/Kbapi.hs
+++ src/Kbapi.hs
@@ -1,11 +1,40 @@
 -- |
 --
--- * The [abstract ayntax tree](https://en.wikipedia.org/wiki/Abstract_syntax_tree) ( ast )
---   aims to be a data structure able to:
+-- This package defines the API for the /knowledge base/ used by [dhscanner](https://dhscanner.org/docs/).
 --
---     * represent /multiple/ ( native ) ast kinds
---     * from /various/ programming languages
+-- * Documentation is intended for both humans🚶and LLM agents 🤖.
 --
+-- * In this context, a knowledge base is a structured collection of /code facts/.
+--    
+-- * Code facts form an /abstraction layer/ over a /data representation/.
+--
+-- Data representations come in /many/ forms, for example:
+--
+-- * Graph databases
+-- * Relational databases
+-- * Logic programs
+-- * Datalog programs
+--
+-- The design aims to /decouple/ (as much as possible) the layers:
+--
+-- * Data representation
+-- * Code facts abstraction
+-- * Knowledge base API
+--
+-- For instance, migrating from a graph database to a logic program representation
+--
+-- * conceptually straightforward
+--
+-- As another example, changes to the knowledge base API
+--
+-- * ideally do not require fundamental modifications of code facts
+--
+-- ==== Learn more 💡
+--
+-- For a detailed explanation of code facts and how they are generated,
+-- see [dhscanner-kbgen](https://hackage.haskell.org/package/dhscanner-kbgen)
+-- on [Hackage](https://hackage.haskell.org/).
+
 
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
